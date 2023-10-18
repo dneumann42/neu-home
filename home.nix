5ac78651fac8d39e27f7f0eec2d70ee31ab2f72a
dneumann <dneumann@dneumann-92.lan>
2023-10-17 22:13:59 -0400

    adding and configuring packages

--- home.nix
+++ home.nix
@@ -26,8 +26,14 @@ in
     killall
     arandr
     emacs29
+    unstable.neovim
     pavucontrol
     neofetch
+    dmenu
+    i3status
+    i3lock
+    i3blocks
+    kitty
     ripgrep
     yad # calendar and other popups
     love
@@ -40,35 +46,87 @@ in
     "nim2"
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    autocd = true;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    shellAliases = {
+      ll = "ls -la";
+      hello = ''echo " World!"'';
+      update = "sudo nixos-rebuild switch";
+
+      v = "$EDITOR";
+
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gcm = "git commit -m";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ 
+        "git" 
+      ];
+      theme = "robbyrussell";
+    };
+    plugins = [
+      { # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.4.0";
+          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
+    ];
+
+    envExtra = ''
+    export PATH="$PATH:~/.nix-profile/bin/"
+
+    source ~/.nix-profile/etc/profile.d/nix.sh
+    '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/dneumann/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
+  home.file.".config/i3" = {
+    source = ./configs/i3;
+    recursive = true;
+  };
+
+  home.file.".config/picom" = {
+    source = ./configs/picom;
+    recursive = true;
+  };
+
+  home.file.".config/i3blocks" = {
+    source = ./configs/i3blocks;
+    recursive = true;
+  };
+
+  home.file.".config/nvim" = {
+    source = ./configs/nvim;
+    recursive = true;
+  };
+
+  home.file.".config/kitty" = {
+    source = ./configs/kitty;
+    recursive = true;
+  };
+
+  home.file.".config/fontconfig" = {
+    source = ./configs/fontconfig;
+    recursive = true;
+  };
+
+  home.file.".emacs.d" = {
+    source = ./configs/emacs;
+    recursive = true;
+  };
+
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
